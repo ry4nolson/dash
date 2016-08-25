e93a08075685aa60434d9cebac5e3923a48a5b76
Ryan Olson <ry4nolson@gmail.com>
2016-08-24 21:16:52 -0500

    add another chart, don't like this one

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -3,11 +3,11 @@ app.controller("HomeController", function HomeController($scope, $rootScope, Ord
 	$rootScope.home = true;
 	var exports = {
 		loadData: function () {
-			
+
 			window.hours = [];
 			window.amounts = new Array(24);
 			window.compareAmounts = new Array(24);
-			for(var i = 0; i<=23; i++) {
+			for (var i = 0; i <= 23; i++) {
 				hours.push(i);
 				amounts[i] = 0;
 				compareAmounts[i] = 0;
@@ -15,14 +15,33 @@ app.controller("HomeController", function HomeController($scope, $rootScope, Ord
 
 			var orderCanvas = $(".order-chart")[0].getContext('2d');
 
-			function buildOrdersChart(){
+			function buildOrdersChart() {
 				var chart = new Chart(orderCanvas, {
-					type:'line',
-					data:{
-						labels:hours,
+					type: 'line',
+					data: {
+						labels: hours,
 						datasets: [
-							{ data: amounts, label: $rootScope.date.toLocaleDateString() }, 
-							{ data: compareAmounts, label: $rootScope.comparedate.toLocaleDateString()}]
+							{ 
+								data: amounts, 
+								label: $rootScope.date.toLocaleDateString(), 
+								backgroundColor:"rgba(43,42,12,0.1)" 
+							},
+							{ 
+								data: compareAmounts, 
+								label: $rootScope.comparedate.toLocaleDateString(),
+								backgroundColor:"rgba(234,66,0,0.1)"
+							}]
+					},
+					options: {
+						scales: {
+							yAxes: [{
+                ticks: {
+									callback: function (value, index, values) {
+										return "$" + value;
+									}
+                }
+							}]
+						}
 					}
 				});
 			}
@@ -39,16 +58,16 @@ app.controller("HomeController", function HomeController($scope, $rootScope, Ord
 				}
 
 				var runningTotal = 0;
-				for(var i in $scope.orders){
+				for (var i in $scope.orders) {
 					var d = new Date($scope.orders[i].created_at);
 					var h = d.getHours();
 					runningTotal += $scope.orders[i].grand_total;
 					amounts[h] = runningTotal;
 				}
 
-				for (var i = 1; i<=23; i++){
+				for (var i = 1; i <= 23; i++) {
 					if (amounts[i] == 0)
-						amounts[i] = amounts[i-1];
+						amounts[i] = amounts[i - 1];
 				}
 
 				buildOrdersChart();
@@ -56,7 +75,7 @@ app.controller("HomeController", function HomeController($scope, $rootScope, Ord
 				$scope.coupons = 0;
 				$scope.adcodes = 0;
 
-				for (var order in $scope.orders){
+				for (var order in $scope.orders) {
 					if ($scope.orders[order].coupon_code != "")
 						$scope.coupons++;
 					if ($scope.orders[order].adcode != "")
@@ -76,26 +95,26 @@ app.controller("HomeController", function HomeController($scope, $rootScope, Ord
 				var comparedate = $rootScope.comparedate;
 				var comparedateNow = $rootScope.comparedateNow;
 
-				for(var order in orders.orders){
+				for (var order in orders.orders) {
 					var current = orders.orders[order];
 					var orderDate = new Date(current.ordered_at);
 					//console.log(orderDate, comparedate);
-					if (orderDate.getTime() < comparedateNow.getTime()){
+					if (orderDate.getTime() < comparedateNow.getTime()) {
 						$scope.compare_order_count_tonow++;
 						$scope.compare_order_total_tonow += current.grand_total;
 					}
 				}
 				var runningTotal = 0;
-				for(var i in orders.orders){
+				for (var i in orders.orders) {
 					var d = new Date(orders.orders[i].created_at);
 					var h = d.getHours();
 					runningTotal += orders.orders[i].grand_total;
 					compareAmounts[h] = runningTotal;
 				}
 
-				for (var i = 1; i<=23; i++){
+				for (var i = 1; i <= 23; i++) {
 					if (compareAmounts[i] == 0)
-						compareAmounts[i] = compareAmounts[i-1];
+						compareAmounts[i] = compareAmounts[i - 1];
 				}
 
 				buildOrdersChart();
@@ -105,7 +124,7 @@ app.controller("HomeController", function HomeController($scope, $rootScope, Ord
 
 			OrderItemsFactory.getOrderItemsForToday().then(function (data) {
 				var orders = data.orders;
-				var items = {};
+				window.items = {};
 				var total = 0;
 				for (var order in orders) {
 					for (var item in orders[order].items) {
@@ -125,6 +144,32 @@ app.controller("HomeController", function HomeController($scope, $rootScope, Ord
 						total += oItem.quantity;
 					}
 				}
+
+				var itemNames = [], itemTotals = [], itemQuantities = [];
+				for (var i in items) {
+					itemNames.push(i);
+					itemTotals.push(items[i].total);
+					itemQuantities.push(items[i].qty);
+				}
+
+				var itemsCanvas = $(".items-chart")[0].getContext('2d');
+
+				var chart = new Chart(itemsCanvas, {
+					type: 'bar',
+					data: {
+						labels: itemNames,
+						datasets: [{
+							label: 'Item Totals',
+							data: itemTotals
+						}/*,
+						{
+							label: 'Item Quantities',
+							data: itemQuantities
+						}*/]
+					}
+				});
+
+				console.log(itemQuantities);
 				$scope.total = total;
 				$scope.items = items;
 			});
@@ -138,22 +183,22 @@ app.controller("HomeController", function HomeController($scope, $rootScope, Ord
 					if (data.orders.length == 1) $scope.newCustomers++;
 					else $scope.repeatCustomers++;
 
-					if ($scope.newCustomers + $scope.repeatCustomers == $scope.order_count){
+					if ($scope.newCustomers + $scope.repeatCustomers == $scope.order_count) {
 						var chart = new Chart(customerCanvas, {
 							type: 'pie',
-							data: { 
-								labels:['New', 'Repeat'],
-								datasets :[ {
+							data: {
+								labels: ['New', 'Repeat'],
+								datasets: [{
 									data: [$scope.newCustomers, $scope.repeatCustomers],
-									backgroundColor : ["#f00", "#00f"]
+									backgroundColor: ["#f00", "#00f"]
 								}]
 							}
 						});
 					}
 				});
 			}
-			
-			ContentFactory.getReviews().then(function(data){
+
+			ContentFactory.getReviews().then(function (data) {
 				$scope.reviews = data.product_reviews;
 			})
 		}
