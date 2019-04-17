00f826ca22fa3da80e37008a6e665baa128745db
Ryan Olson <ry4nolson@gmail.com>
2019-04-17 18:30:07 -0500

    this might be it

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -29,7 +29,7 @@ app.controller("LoginController", function LoginController($scope, $rootScope, $
 			error: function() {
 				$rootScope.doLogin();
 			}
-		}).then(handleAuth);
+		}).then(handleAuth, $rootScope.doLogin);
 	}
 	
 	$rootScope.doLogin = function () {
